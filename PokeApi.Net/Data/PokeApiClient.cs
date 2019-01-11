30d0b581a81bfbacf54fe3cc485c097eb7a1c0f4
JJ Banda <mtrdp642@gmail.com>
2019-01-11 13:28:45 -0500

    created api call methods

--- PokeApi.Net/Data/PokeApiClient.cs
+++ PokeApi.Net/Data/PokeApiClient.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PokeApi.Net.Data
 {
@@ -15,26 +16,43 @@ namespace PokeApi.Net.Data
         private HttpClient client;
 
         /// <summary>
-        /// 
+        /// Constructor
         /// </summary>
-        /// <param name="messageHandler"></param>
+        /// <param name="messageHandler">Message handler implementation</param>
         public PokeApiClient(HttpMessageHandler messageHandler)
         {
             client = new HttpClient(messageHandler);
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public async System.Threading.Tasks.Task<T> GetResourceAsync<T>() where T : Resource
+        private async Task<T> GetResourcesWithParamsAsync<T>(string apiParam) where T : Resource
         {
             string apiEndpoint = typeof(T).GetField("ApiEndpoint").GetValue(null).ToString();
-            HttpResponseMessage response = await client.GetAsync(apiEndpoint);
+            HttpResponseMessage response = await client.GetAsync($"{apiEndpoint}/{apiParam}");
 
             string resp = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<T>(resp);
+        }
+
+        /// <summary>
+        /// Gets a resource by id
+        /// </summary>
+        /// <typeparam name="T">The type of resource</typeparam>
+        /// <param name="id">Id of resource</param>
+        /// <returns>The object of the resource</returns>
+        public async Task<T> GetResourceAsync<T>(int id) where T : Resource
+        {
+            return await GetResourcesWithParamsAsync<T>(id.ToString());
+        }
+
+        /// <summary>
+        /// Gets a resource by name
+        /// </summary>
+        /// <typeparam name="T">The type of resource</typeparam>
+        /// <param name="name">Name of resource</param>
+        /// <returns>The object of the resource</returns>
+        public async Task<T> GetResourceAsync<T>(string name) where T : Resource
+        {
+            return await GetResourcesWithParamsAsync<T>(name);
         }
     }
 }
