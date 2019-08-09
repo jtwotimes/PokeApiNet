baa603646831691267a5abd7e2fc166c1b6e1dfa
Jose Toledo <jotatoledon@gmail.com>
2019-08-09 13:12:05 +0200

    feat(cache): add resource list cache manager
    * Add an implementation of cache manager
    that controls the caching of resource list instances

---
+++ PokeApiNet.Tests/Cache/ResourceListCacheManagerTests.cs
@@ -0,0 +1,193 @@
+﻿using PokeApiNet.Cache;
+using PokeApiNet.Models;
+using System;
+using Xunit;
+
+namespace PokeApiNet.Tests.Cache
+{
+    public class ResourceListCacheManagerTests
+    {
+        private readonly string testUri;
+
+        public ResourceListCacheManagerTests()
+        {
+            testUri = "unit-test";
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetApiResources_WithStoredUri_ReturnsResource()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            (string uri, ApiResourceList<Machine> list) = CreateFakeApiResourceList<Machine>();
+            sut.Store(uri, list);
+
+            // act
+            ApiResourceList<Machine> cached = sut.GetApiResources<Machine>(uri);
+
+            // assert
+            Assert.Same(list, cached);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetApiResources_WithNonStoredUri_ReturnsNull()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            (string uri, ApiResourceList<Machine> list) = CreateFakeApiResourceList<Machine>();
+            sut.Store(uri, list);
+
+            // act
+            ApiResourceList<Machine> cached = sut.GetApiResources<Machine>(testUri);
+
+            // assert
+            Assert.Null(cached);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetApiResources_OnEmptyCache_ReturnsNull()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+
+            // act
+            ApiResourceList<Machine> cached = sut.GetApiResources<Machine>(testUri);
+
+            // assert
+            Assert.Null(cached);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetNamedResources_WithStoredUri_ReturnsResource()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            (string uri, NamedApiResourceList<Berry> list) = CreateFakeNamedResourceList<Berry>();
+            sut.Store(uri, list);
+
+            // act
+            NamedApiResourceList<Berry> cached = sut.GetNamedResources<Berry>(uri);
+
+            // assert
+            Assert.Same(list, cached);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetNamedResources_WithNonStoredUri_ReturnsNull()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            (string uri, NamedApiResourceList<Berry> list) = CreateFakeNamedResourceList<Berry>();
+            sut.Store(uri, list);
+
+            // act
+            NamedApiResourceList<Berry> cached = sut.GetNamedResources<Berry>(testUri);
+
+            // assert
+            Assert.Null(cached);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetNamedResources_OnEmptyCache_ReturnsNull()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+
+            // act
+            NamedApiResourceList<Berry> cached = sut.GetNamedResources<Berry>(testUri);
+
+            // assert
+            Assert.Null(cached);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void AllCacheIsCleared()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            (string berryUri, NamedApiResourceList<Berry> berryList) = CreateFakeNamedResourceList<Berry>();
+            (string machineUri, ApiResourceList<Machine> machineList) = CreateFakeApiResourceList<Machine>();
+            sut.Store(berryUri, berryList);
+            sut.Store(machineUri, machineList);
+
+            // act
+            sut.ClearAll();
+
+            // assert
+            NamedApiResourceList<Berry> cacheddBerryList = sut.GetNamedResources<Berry>(berryUri);
+            ApiResourceList<Machine> cachedMachineList = sut.GetApiResources<Machine>(machineUri);
+            Assert.Null(cacheddBerryList);
+            Assert.Null(cachedMachineList);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void CacheIsClearedForSpecificType()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            (string berryUri, NamedApiResourceList<Berry> berryList) = CreateFakeNamedResourceList<Berry>();
+            (string machineUri, ApiResourceList<Machine> machineList) = CreateFakeApiResourceList<Machine>();
+            sut.Store(berryUri, berryList);
+            sut.Store(machineUri, machineList);
+
+            // act
+            sut.Clear<Berry>();
+
+            // assert
+            NamedApiResourceList<Berry> cachedBerryList = sut.GetNamedResources<Berry>(berryUri);
+            ApiResourceList<Machine> cachedMachineList = sut.GetApiResources<Machine>(machineUri);
+            Assert.Null(cachedBerryList);
+            Assert.NotNull(cachedMachineList);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void StoreThrowsIfTypeNotSupported()
+        {
+            // assemble
+            ResourceListCacheManager sut = CreateSut();
+            TestResourceList list = new TestResourceList();
+
+            // assert
+            Assert.Throws<NotSupportedException>(() =>
+            {
+                // act
+                sut.Store(testUri, list);
+            });
+        }
+
+        private (string, ApiResourceList<T>) CreateFakeApiResourceList<T>(string uri = null)
+            where T : ApiResource
+        {
+            return (uri ?? typeof(T).Name, new ApiResourceList<T>());
+        }
+
+        private (string, NamedApiResourceList<T>) CreateFakeNamedResourceList<T>(string uri = null)
+            where T : NamedApiResource
+        {
+            return (uri ?? typeof(T).Name, new NamedApiResourceList<T>());
+        }
+
+        private static ResourceListCacheManager CreateSut()
+        {
+            return new ResourceListCacheManager();
+        }
+
+        private class TestResourceList : ResourceList<TestResource> { }
+
+        private class TestResource : ResourceBase
+        {
+            public override int Id { get; set; } = 1;
+
+            public new static string ApiEndpoint => "";
+        };
+    }
+}
---
+++ PokeApiNet/Cache/ResourceListCacheManager.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using PokeApiNet.Models;
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+
+namespace PokeApiNet.Cache
+{
+    /// <summary>
+    /// Manages caches for instances of subclasses from <see cref="ResourceList{T}"/>
+    /// </summary>
+    internal sealed class ResourceListCacheManager : BaseCacheManager
+    {
+        private readonly IImmutableDictionary<System.Type, ListCache> listCaches;
+
+        public ResourceListCacheManager()
+        {
+            // TODO allow configuration of experiation policies
+            this.listCaches = ResourceTypes.ToImmutableDictionary(x => x, _ => new ListCache());
+        }
+
+        /// <summary>
+        /// Caches a value
+        /// </summary>
+        /// <typeparam name="T">Type of object to be cached</typeparam>
+        /// <param name="uri">The relative path associated to the resource list</param>
+        /// <param name="obj">The resource list to cache</param>
+        /// <exception cref="NotSupportedException">
+        /// The given type is not supported for searching via PokeAPI
+        /// </exception>
+        public void Store<T>(string uri, ResourceList<T> obj)
+            where T : ResourceBase
+        {
+            System.Type resourceType = typeof(T);
+            if (!IsTypeSupported(resourceType))
+            {
+                throw new NotSupportedException($"{resourceType.FullName} is not supported.");
+            }
+
+            listCaches[resourceType].Store(uri, obj);
+        }
+
+        public ApiResourceList<T> GetApiResources<T>(string uri) where T : ApiResource
+        {
+            System.Type resourceType = typeof(T);
+            return listCaches[resourceType].Get<T>(uri) as ApiResourceList<T>;
+        }
+
+        public NamedApiResourceList<T> GetNamedResources<T>(string uri) where T : NamedApiResource
+        {
+            System.Type resourceType = typeof(T);
+            return listCaches[resourceType].Get<T>(uri) as NamedApiResourceList<T>;
+        }
+
+        /// <summary>
+        /// Clears all caches
+        /// </summary>
+        public void ClearAll()
+        {
+            foreach (ListCache cache in listCaches.Values)
+            {
+                cache.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Clears a specific cache
+        /// </summary>
+        /// <typeparam name="T">The type of cache to clear</typeparam>
+        public void Clear<T>() where T : ResourceBase
+        {
+            System.Type type = typeof(T);
+            listCaches[type].Clear();
+        }
+
+        private sealed class ListCache
+        {
+            private readonly MemoryCache uriCache;
+
+            public ListCache()
+            {
+                // TODO allow configuration of expiration policies
+                uriCache = new MemoryCache(new MemoryCacheOptions());
+                this.ClearToken = new CancellationTokenSource();
+            }
+
+            private CancellationTokenSource ClearToken { get; set; }
+
+            public void Store<T>(string uri, ResourceList<T> resourceList)
+                where T : ResourceBase
+            {
+                uriCache.Set(uri, resourceList, CreateEntryOptions());
+            }
+
+            /// <summary>
+            /// Clears all cache data
+            /// </summary>
+            public void Clear()
+            {
+                // TODO add lock?
+                if (ClearToken != null && !ClearToken.IsCancellationRequested && ClearToken.Token.CanBeCanceled)
+                {
+                    ClearToken.Cancel();
+                    ClearToken.Dispose();
+                }
+
+                ClearToken = new CancellationTokenSource();
+            }
+
+            /// <remarks>
+            /// New options instance has to be constantly instantiated instead of shared
+            /// as a consecuence of <see cref="ClearToken"/> being mutable
+            /// </remarks>
+            private MemoryCacheEntryOptions CreateEntryOptions() => new MemoryCacheEntryOptions()
+                    .AddExpirationToken(new CancellationChangeToken(ClearToken.Token));
+
+            public ResourceList<T> Get<T>(string uri) where T : ResourceBase => uriCache.Get<ResourceList<T>>(uri);
+        }
+    }
+}
