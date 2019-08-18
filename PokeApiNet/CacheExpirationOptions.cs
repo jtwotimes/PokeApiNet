9bcaafe07574451a907674c5135a95d6dfc4c161
Jose Toledo <jotatoledon@gmail.com>
2019-08-18 22:13:11 +0200

    chore(cache): validate cache expiration options props

--- PokeApiNet/CacheExpirationOptions.cs
+++ PokeApiNet/CacheExpirationOptions.cs
@@ -3,10 +3,13 @@
 namespace PokeApiNet
 {
     /// <summary>
-    /// Represents the cache expiration options applied to a cache entry.
+    /// Represents the cache expiration options of a cache entry.
     /// </summary>
     public class CacheExpirationOptions
     {
+        private TimeSpan? _absoluteExpirationRelativeToNow;
+        private TimeSpan? _slidingExpiration;
+
         /// <summary>
         /// Gets or sets an absolute expiration date for a cache entry.
         /// </summary>
@@ -15,12 +18,50 @@ namespace PokeApiNet
         /// <summary>
         /// Gets or sets an absolute expiration time, relative to now.
         /// </summary>
-        public TimeSpan? AbsoluteExpirationRelativeToNow { get; set; }
+        /// <summary>
+        /// Gets or sets an absolute expiration time, relative to now.
+        /// </summary>
+        public TimeSpan? AbsoluteExpirationRelativeToNow
+        {
+            get
+            {
+                return _absoluteExpirationRelativeToNow;
+            }
+            set
+            {
+                if (value <= TimeSpan.Zero)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(AbsoluteExpirationRelativeToNow),
+                        value,
+                        "The relative expiration value must be positive.");
+                }
+
+                _absoluteExpirationRelativeToNow = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets how long a cache entry can be inactive (e.g. not accessed) before it will be removed.
         /// This will not extend the entry lifetime beyond the absolute expiration (if set).
         /// </summary>
-        public TimeSpan? SlidingExpiration { get; set; }
+        public TimeSpan? SlidingExpiration
+        {
+            get
+            {
+                return _slidingExpiration;
+            }
+            set
+            {
+                if (value <= TimeSpan.Zero)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(SlidingExpiration),
+                        value,
+                        "The sliding expiration value must be positive.");
+                }
+                _slidingExpiration = value;
+            }
+        }
     }
 }
